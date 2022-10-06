0a5ff70325a2e75e174d4fb2fd0222fe1bed8d4f
Mekel Ilyasa <mekel@hybrid.is>
2022-10-06 22:32:51 +0700

    feat: finishing

--- script.js
+++ script.js
@@ -1,58 +1,79 @@
+const btnInput = document.getElementById('btn-search');
+const queryInput = document.getElementById('query');
+
 const handleSearch = async (event) => {
 	const results = document.getElementById('results');
 	results.innerHTML = '';
-
-	const query = document.getElementById('query').value;
 
 	const data = await fetch('./data.json')
 		.then((res) => res.json())
 		.catch((err) => console.log(err));
 
+	const query = queryInput.value.toLowerCase();
+
 	const filteredData = data.filter(
 		(item) =>
-			item.nama_lengkap.toLowerCase().includes(query.toLowerCase()) ||
-			item.nama_panggilan.toLowerCase().includes(query.toLowerCase()) ||
-			item.nim.toLowerCase().includes(query.toLowerCase()) ||
-			item.nomor_telepon.toLowerCase().includes(query.toLowerCase()) ||
-			item.id_line.toLowerCase().includes(query.toLowerCase()) ||
-			item.email.toLowerCase().includes(query.toLowerCase())
+			item.nama_lengkap.toLowerCase().includes(query) ||
+			item.nama_panggilan.toLowerCase().includes(query) ||
+			item.nim.toString().toLowerCase().includes(query)
 	);
 
-	filteredData.forEach((item) => {
-		const namaLengkap = document.createElement('h1');
-		namaLengkap.innerText = item.nama_lengkap;
-		results.appendChild(namaLengkap);
+	if (!query) {
+		const enterQuery = document.createElement('p');
+		enterQuery.innerText = 'Masukan kata kunci pencarian';
+		results.appendChild(enterQuery);
+	} else if (filteredData.length > 0) {
+		filteredData.forEach((item) => {
+			const card = document.createElement('div');
+			card.classList.add('card');
 
-		const namaPanggilan = document.createElement('p');
-		namaPanggilan.innerText = `Nama lengkap : ${item.nama_panggilan}`;
-		results.appendChild(namaPanggilan);
+			const namaLengkap = document.createElement('h1');
+			namaLengkap.innerText = item.nama_lengkap;
+			card.appendChild(namaLengkap);
 
-		const nim = document.createElement('p');
-		nim.innerText = `NIM : ${item.nim}`;
-		results.appendChild(nim);
+			const namaPanggilan = document.createElement('p');
+			namaPanggilan.innerText = `Nama lengkap : ${item.nama_panggilan}`;
+			card.appendChild(namaPanggilan);
 
-		const nomorTelepon = document.createElement('p');
-		nomorTelepon.innerText = `Nomor Telepon : ${item.nomor_telepon}`;
-		results.appendChild(nomorTelepon);
+			const nim = document.createElement('p');
+			nim.innerText = `NIM : ${item.nim}`;
+			card.appendChild(nim);
 
-		const idLine = document.createElement('p');
-		idLine.innerText = `ID Line : ${item.id_line}`;
-		results.appendChild(idLine);
+			const nomorTelepon = document.createElement('p');
+			nomorTelepon.innerText = `Nomor Telepon : ${item.nomor_telepon}`;
+			card.appendChild(nomorTelepon);
 
-		const email = document.createElement('p');
-		email.innerText = `Email : ${item.email}`;
-		results.appendChild(email);
+			const idLine = document.createElement('p');
+			idLine.innerText = `ID Line : ${item.id_line}`;
+			card.appendChild(idLine);
 
-		const hobi = document.createElement('p');
-		hobi.innerText = `Hobi : ${item.hobi}`;
-		results.appendChild(hobi);
+			const email = document.createElement('p');
+			email.innerText = `Email : ${item.email}`;
+			card.appendChild(email);
 
-		const tanggalLahir = document.createElement('p');
-		tanggalLahir.innerText = `Tanggal Lahir : ${item.tanggal_lahir}`;
-		results.appendChild(tanggalLahir);
-	});
+			const hobi = document.createElement('p');
+			hobi.innerText = `Hobi : ${item.hobi}`;
+			card.appendChild(hobi);
+
+			const tanggalLahir = document.createElement('p');
+			tanggalLahir.innerText = `Tanggal Lahir : ${item.tanggal_lahir}`;
+			card.appendChild(tanggalLahir);
+
+			results.appendChild(card);
+		});
+	} else {
+		const notFound = document.createElement('p');
+		notFound.innerText = 'Data tidak ditemukan';
+		results.appendChild(notFound);
+	}
 };
 
-const btnInput = document.getElementById('btn-search');
+const handleEnter = (event) => {
+	if (event.key === 'Enter') {
+		event.preventDefault();
+		btnInput.click();
+	}
+};
 
 btnInput.addEventListener('click', handleSearch);
+queryInput.addEventListener('keypress', handleEnter);
